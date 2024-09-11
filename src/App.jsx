a9b1183140a2b1f81f8c2ef6d573ce0c8fe1f64d
Cleone <cleonefoo@gmail.com>
2024-09-11 23:08:51 +0800

    Navbar Creation

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./NavBar/navbar";
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
