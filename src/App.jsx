71fcc92ec460019f16b0f19e2342906eed57f887
Cleone <cleonefoo@gmail.com>
2024-09-13 22:20:48 +0800

    navber modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Navbar from "./NavBar/navbar";
 import React from "react";
+import Intro from "./components/Intro/intro";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Intro />
     </div>
   );
 }
